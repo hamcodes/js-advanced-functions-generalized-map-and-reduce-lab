b3dff09c4ee9fe7d13de537c291b576a7fafdff1
github username <github email address>
2020-03-31 16:29:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(sourceArray, funct) {
+  let result = []
+  for (const element of sourceArray) {
+      result.push(funct(element))
+  }
+  return result
+}
+
+function reduce(sourceArray, funct, staringPoint =0) {
+  let total;
+  if (staringPoint) {
+      total = staringPoint
+  } else {
+      total = sourceArray[0]
+      sourceArray.shift()
+  }
+  sourceArray.forEach(elem => {
+      total = funct(elem, total)
+  })
+  return total
+}
